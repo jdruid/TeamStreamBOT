c2aa42a2bf99ce48b50ef875ad35626fdab371df
jdruid <joshdrew@outlook.com>
2017-05-15 20:51:29 -0400

    Keys

---
+++ TeamStreamFunctions/Keys.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamStreamFunctions
+{
+    public class Keys
+    {
+        //Blob storage keys
+        public static readonly string baseUrl = "";
+        public static readonly string storageConnectionString = "";
+        public static readonly string thumbnailContainer = "";
+        public static readonly string videoContainer = "";
+        //Computer vision api
+        public static readonly string visionApiURL = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description&language=en";
+        public static readonly string cogservAPIKey = "";
+        //API for teamstream
+        public static readonly string videoApiURL = "";
+    }
+}
